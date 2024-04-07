2512ec4d71892a46198395a96ec8dfce7cb7b6fc
WEIBIN SONG <sone@silmoon.com>
2024-04-07 15:11:28 +0800

    更新

--- WinFormsAot/Form1.cs
+++ WinFormsAot/Form1.cs
@@ -9,7 +9,7 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show(a.ToString());
+            MessageBox.Show("Hello world.");
         }
     }
 }
